f2c226ca45c559a2f1811fa1b1a69b26ed761e16
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-05-05 15:03:24 +0700

    Sửa lại Model và thêm Chủ đề bài quiz

--- Models/ChuDeBaiQuiz.cs
+++ Models/ChuDeBaiQuiz.cs
@@ -22,6 +22,7 @@ namespace ELF.Models
     
         public int maChuDe { get; set; }
         public string tenChuDe { get; set; }
+        public string hinhAnh { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CauHoi> CauHois { get; set; }

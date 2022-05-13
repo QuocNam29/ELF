5e61dd22677c999d152aafe41b7fb8cd69006ea8
197PM33932 - TRẦN QUỐC NAM - K25T-PM1 <nam.197pm33932@vanlanguni.vn>
2022-05-14 00:03:13 +0700

    add template Admin

---
+++ Areas/Admin/Controllers/QuaTangsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ELF.Models;
+
+namespace ELF.Areas.Admin.Controllers
+{
+    public class QuaTangsController : Controller
+    {
+        private ELFVanLang2021Entities db = new ELFVanLang2021Entities();
+
+        // GET: Admin/QuaTangs
+        public ActionResult Index()
+        {
+            return View(db.QuaTangs.ToList());
+        }
+
+        // GET: Admin/QuaTangs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuaTang quaTang = db.QuaTangs.Find(id);
+            if (quaTang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quaTang);
+        }
+
+        // GET: Admin/QuaTangs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/QuaTangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "maQuaTang,tenQuaTang,diemDoi,trangThai,ngayTao,ngayThayDoi,ghiChu,hinhAnh")] QuaTang quaTang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.QuaTangs.Add(quaTang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(quaTang);
+        }
+
+        // GET: Admin/QuaTangs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuaTang quaTang = db.QuaTangs.Find(id);
+            if (quaTang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quaTang);
+        }
+
+        // POST: Admin/QuaTangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "maQuaTang,tenQuaTang,diemDoi,trangThai,ngayTao,ngayThayDoi,ghiChu,hinhAnh")] QuaTang quaTang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(quaTang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(quaTang);
+        }
+
+        // GET: Admin/QuaTangs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuaTang quaTang = db.QuaTangs.Find(id);
+            if (quaTang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quaTang);
+        }
+
+        // POST: Admin/QuaTangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            QuaTang quaTang = db.QuaTangs.Find(id);
+            db.QuaTangs.Remove(quaTang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

8f9356af020ba0abc4ca701967fc0880d892e008
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-05-18 12:17:44 +0700

    Làm validation cho Import file excel bài quiz

--- Areas/Admin/Controllers/AddCauHoisController.cs
+++ Areas/Admin/Controllers/AddCauHoisController.cs
@@ -25,100 +25,112 @@ namespace ELF.Areas.Admin.Controllers
             if (postedFile != null)
             {
                 string path = Server.MapPath("~/Uploads/");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-
                 filePath = path + Path.GetFileName(postedFile.FileName);
                 string extension = Path.GetExtension(postedFile.FileName);
-                postedFile.SaveAs(filePath);
 
-                string conString = string.Empty;
-                switch (extension)
+                if (extension.ToLower() == ".xls" || extension.ToLower() == ".xlsx")
                 {
-                    case ".xls": //Excel 97-03.
-                        conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
-                        break;
-                    case ".xlsx": //Excel 07 and above.
-                        conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
-                        break;
-                }
-
-                DataTable dt = new DataTable();
-                conString = string.Format(conString, filePath);
-
-                using (OleDbConnection connExcel = new OleDbConnection(conString))
-                {
-                    using (OleDbCommand cmdExcel = new OleDbCommand())
+                    if (!Directory.Exists(path))
                     {
-                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                        Directory.CreateDirectory(path);
+                    }
+
+                    postedFile.SaveAs(filePath);
+
+                    string conString = string.Empty;
+                    switch (extension)
+                    {
+                        case ".xls": //Excel 97-03.
+                            conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                            break;
+                        case ".xlsx": //Excel 07 and above.
+                            conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                            break;
+                    }
+
+                    DataTable dt = new DataTable();
+                    conString = string.Format(conString, filePath);
+
+                    using (OleDbConnection connExcel = new OleDbConnection(conString))
+                    {
+                        using (OleDbCommand cmdExcel = new OleDbCommand())
                         {
-                            cmdExcel.Connection = connExcel;
+                            using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                            {
+                                cmdExcel.Connection = connExcel;
 
-                            //Get the name of First Sheet.
-                            connExcel.Open();
-                            DataTable dtExcelSchema;
-                            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                            
-                            string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
-                                
-                            connExcel.Close();
+                                //Get the name of First Sheet.
+                                connExcel.Open();
+                                DataTable dtExcelSchema;
+                                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
 
-                            //Read Data from First Sheet.
-                            connExcel.Open();
-                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
-                            odaExcel.SelectCommand = cmdExcel;
-                            odaExcel.Fill(dt);
-                            connExcel.Close();                                                                                            
-                          
+                                string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+
+                                connExcel.Close();
+
+                                //Read Data from First Sheet.
+                                connExcel.Open();
+                                cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                odaExcel.SelectCommand = cmdExcel;
+                                odaExcel.Fill(dt);
+                                connExcel.Close();
+
+                            }
                         }
                     }
-                }
-                //Insert records to database table.
-                foreach (DataRow row in dt.Rows)
-                {
-                    db.CauHois.Add(new CauHoi
+                    //Insert records to database table.
+                    foreach (DataRow row in dt.Rows)
                     {
-                        noiDungCauHoi = row["noiDungCauHoi"].ToString(),
-                        ngayTao = System.DateTime.Now,
-                        ngayThayDoi = System.DateTime.Now,
-                        maChuDe = int.Parse(row["maChuDe"].ToString())
-                    });
+                        db.CauHois.Add(new CauHoi
+                        {
+                            noiDungCauHoi = row["noiDungCauHoi"].ToString(),
+                            ngayTao = System.DateTime.Now,
+                            ngayThayDoi = System.DateTime.Now,
+                            maChuDe = int.Parse(row["maChuDe"].ToString())
+                        });
+                        db.SaveChanges();
+                        int lastProductId = db.CauHois.Max(item => item.maCauHoi);
+
+                        db.DapAns.Add(new DapAn
+                        {
+                            maCauHoi = lastProductId,
+                            NoiDungDapAn = row["dapAnA"].ToString(),
+                            dapAn1 = bool.Parse(row["ATrue"].ToString())
+
+                        });
+                        db.DapAns.Add(new DapAn
+                        {
+                            maCauHoi = lastProductId,
+                            NoiDungDapAn = row["dapAnB"].ToString(),
+                            dapAn1 = bool.Parse(row["BTrue"].ToString())
+
+                        });
+                        db.DapAns.Add(new DapAn
+                        {
+                            maCauHoi = lastProductId,
+                            NoiDungDapAn = row["dapAnC"].ToString(),
+                            dapAn1 = bool.Parse(row["CTrue"].ToString())
+
+                        });
+                        db.DapAns.Add(new DapAn
+                        {
+                            maCauHoi = lastProductId,
+                            NoiDungDapAn = row["dapAnD"].ToString(),
+                            dapAn1 = bool.Parse(row["DTrue"].ToString())
+
+                        });
+                    }
                     db.SaveChanges();
-                    int lastProductId = db.CauHois.Max(item => item.maCauHoi);
-
-                    db.DapAns.Add(new DapAn
-                    {
-                        maCauHoi = lastProductId,
-                        NoiDungDapAn = row["dapAnA"].ToString(),
-                        dapAn1 = bool.Parse(row["ATrue"].ToString())
-
-                    });
-                    db.DapAns.Add(new DapAn
-                    {
-                        maCauHoi = lastProductId,
-                        NoiDungDapAn = row["dapAnB"].ToString(),
-                        dapAn1 = bool.Parse(row["BTrue"].ToString())
-
-                    });
-                    db.DapAns.Add(new DapAn
-                    {
-                        maCauHoi = lastProductId,
-                        NoiDungDapAn = row["dapAnC"].ToString(),
-                        dapAn1 = bool.Parse(row["CTrue"].ToString())
-
-                    });
-                    db.DapAns.Add(new DapAn
-                    {
-                        maCauHoi = lastProductId,
-                        NoiDungDapAn = row["dapAnD"].ToString(),
-                        dapAn1 = bool.Parse(row["DTrue"].ToString())
-
-                    });
                 }
-                db.SaveChanges();
+                else
+                {
+                    ViewBag.FileStatus = "Định dạng file không hợp lệ!";
+                }
 
+            }
+            else
+            {
+                ViewBag.FileStatus = "Bạn chưa chọn file để import!";
             }
 
             return View();

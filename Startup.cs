9291fab2ac27a0fc775d35d2c4e667a0ece0ff4d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-05-20 16:43:55 +0700

    Sửa lại chế độ Authentication của project để đăng nhập bằng email Văn Lang

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELF.Startup))]
+namespace ELF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

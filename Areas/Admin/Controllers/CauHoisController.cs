300642d052bd001f86bd03f946c4fbd02f491050
197PM33932 - TRẦN QUỐC NAM - K25T-PM1 <nam.197pm33932@vanlanguni.vn>
2022-05-24 13:26:56 +0700

    Thêm câu hỏi

--- Areas/Admin/Controllers/CauHoisController.cs
+++ Areas/Admin/Controllers/CauHoisController.cs
@@ -47,29 +47,79 @@ namespace ELF.Areas.Admin.Controllers
         }
 
         // GET: Admin/CauHois/Create
-        public ActionResult Create()
+        public ActionResult Create(int maCD, string cauHoi, string DA1, string DA2, string DA3, string DA4, string DA_true)
         {
-            ViewBag.maChuDe = new SelectList(db.ChuDeBaiQuizs, "maChuDe", "tenChuDe");
-            return View();
-        }
 
-        // POST: Admin/CauHois/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "maCauHoi,noiDungCauHoi,ngayTao,ngayThayDoi,maChuDe")] CauHoi cauHoi)
-        {
-            if (ModelState.IsValid)
+            db.CauHois.Add(new CauHoi
             {
-                db.CauHois.Add(cauHoi);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                noiDungCauHoi = cauHoi,
+                ngayTao = System.DateTime.Now,
+                maChuDe = maCD
+            });
+            db.SaveChanges();
+            int maCH = db.CauHois.Max(item => item.maCauHoi);
+            bool flat = false;
+            if (DA1 == DA_true)
+            {
+                flat = true;
             }
+            db.DapAns.Add(new DapAn
+            {
+                maCauHoi = maCH,
+                NoiDungDapAn = DA1,
+                dapAn1 = flat
 
-            ViewBag.maChuDe = new SelectList(db.ChuDeBaiQuizs, "maChuDe", "tenChuDe", cauHoi.maChuDe);
-            return View(cauHoi);
+            });
+            if (DA2 == DA_true)
+            {
+                flat = true;
+            }
+            else
+            {
+                flat = false;
+            }
+            db.DapAns.Add(new DapAn
+            {
+                maCauHoi = maCH,
+                NoiDungDapAn = DA1,
+                dapAn1 = flat
+
+            });
+            if (DA3 == DA_true)
+            {
+                flat = true;
+            }
+            else
+            {
+                flat = false;
+            }
+            db.DapAns.Add(new DapAn
+            {
+                maCauHoi = maCH,
+                NoiDungDapAn = DA1,
+                dapAn1 = flat
+
+            });
+            if (DA4 == DA_true)
+            {
+                flat = true;
+            }
+            else
+            {
+                flat = false;
+            }
+            db.DapAns.Add(new DapAn
+            {
+                maCauHoi = maCH,
+                NoiDungDapAn = DA1,
+                dapAn1 = flat
+
+            });
+            db.SaveChanges();
+            return RedirectToAction("Index", new { maCD = maCD });
         }
+
+     
 
         // GET: Admin/CauHois/Edit/5
         public ActionResult Edit(int? id)
@@ -105,30 +155,17 @@ namespace ELF.Areas.Admin.Controllers
         }
 
         // GET: Admin/CauHois/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CauHoi cauHoi = db.CauHois.Find(id);
-            if (cauHoi == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cauHoi);
-        }
-
-        // POST: Admin/CauHois/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int? id, int? maCD)
         {
             CauHoi cauHoi = db.CauHois.Find(id);
+            /* cauHoi.trangThai = "Đã xóa";
+             db.Entry(cauHoi).State = EntityState.Modified;*/
             db.CauHois.Remove(cauHoi);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", new {maCD = maCD });
         }
+
+      
 
         protected override void Dispose(bool disposing)
         {

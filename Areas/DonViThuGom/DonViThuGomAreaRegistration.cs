f15b46a074409be3d32037e51c9c73a78bda5186
197PM33932 - TRẦN QUỐC NAM - K25T-PM1 <nam.197pm33932@vanlanguni.vn>
2022-06-05 15:20:32 +0700

    add đơn vị thu gom

---
+++ Areas/DonViThuGom/DonViThuGomAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ELF.Areas.DonViThuGom
+{
+    public class DonViThuGomAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DonViThuGom";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DonViThuGom_default",
+                "DonViThuGom/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
